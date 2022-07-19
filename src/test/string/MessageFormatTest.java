948d73affc8f6146099e5da837ac67856050d1a0
dksworld1 <dksworld1@gmail.com>
2022-07-19 18:13:35 +0900

    MessageFormat test

--- src/test/string/MessageFormatTest.java
+++ src/test/string/MessageFormatTest.java
@@ -4,7 +4,14 @@ import java.text.MessageFormat;
 
 public class MessageFormatTest {
 
-	public static void main(String[] args) {
+	public void test() {
+		String format = "my name is {0} and my age is {1} and stress guage is upto {2} and say hello to the {3}";
+		
+		
+		System.out.println(MessageFormat.format(format, "kks", 30, 100, "world"));
+	}
+	
+	public void test2() {
 		// TODO Auto-generated method stub
 		String name = "kks";
 		int address = 123456;
@@ -20,6 +27,11 @@ public class MessageFormatTest {
 		System.out.println(String.format(pattern2, name, address, cost));
 		System.out.println(MessageFormat.format(pattern, name, address, cost));
 		System.out.println(MessageFormat.format(pattern3, name, address, cost));
+	}
+	
+	public static void main(String[] args) {
+		MessageFormatTest test = new MessageFormatTest();
+		test.test();
 	}
 
 }
